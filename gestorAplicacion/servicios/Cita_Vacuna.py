282cf4b8c7356c66ad6b7279b53785f099499d5c
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-28 11:24:55 -0500

    Create citavacuna

---
+++ gestorAplicacion/servicios/Cita_Vacuna.py
@@ -0,0 +1,23 @@
+from gestorAplicacion.administracionHospital import Vacuna
+from gestorAplicacion.personas import Paciente
+from gestorAplicacion.servicios import Cita
+#elaborado por Jerónimo
+
+class Cita_Vacuna(Cita):
+    #Atributos e inicializador
+
+    def __init__(self, fecha: str, paciente: Paciente, vacuna: Vacuna):
+        super().__init__(None, fecha, paciente)# El primer argumento de super() se omite al no ser necesario
+        self.vacuna = vacuna
+
+    def validar_pago(self, paciente, id_servicio):
+        for cita_vacuna in paciente.historia_clinica.historial_vacunas:
+            if cita_vacuna.id_servicio == id_servicio:
+                cita_vacuna.estado_pago = True
+
+    def descripcion_servicio(self):
+        return f"{self.id_servicio} - Vacuna: {self.vacuna.nombre} ({self.fecha})"
+    
+    def mensaje(self):
+        return "del servicio de vacunas"
+
