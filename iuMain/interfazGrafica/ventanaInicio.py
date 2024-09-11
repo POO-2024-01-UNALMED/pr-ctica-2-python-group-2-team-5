2ed618549914c01d73a0307abb932e592bfe6717
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-09-11 17:55:33 -0500

    Comienzo de eventos en los Frames

--- iuMain/interfazGrafica/ventanaInicio.py
+++ iuMain/interfazGrafica/ventanaInicio.py
@@ -12,11 +12,18 @@ ventanaInicio.geometry("600x600+400+40")
 
 # ------------ EVENTOS (PRUEBAS) -------------------
 
-#Eventos del menú.
+# Eventos del menú.
 
 def descripcionSistema():
     global mensajeBienvenida
     mensajeBienvenida.config(text = "Puede pedir citas, generar fórmulas y reservar habitaciones...")
+
+#Evento para cambiar el texto en las hojas de vida, Frame P5.
+
+def mostrarHV(event):
+    global hojasDeVida, indice, descripcionDesarrollador
+    descripcionDesarrollador.config(text = hojasDeVida[indice])
+    indice = (indice + 1) % len(hojasDeVida)
 
 # --------------------------------------------------
 
@@ -52,18 +59,34 @@ frameP3.pack(fill = "both", padx = 10, pady = (10, 5))
 
 # Mensaje de bienvenida en el Frame 3.
 
-mensajeBienvenida = Label(frameP3, text = "Bienvenido al Sistema de Información Hospitalario!")
-mensajeBienvenida.place(y = 80)
+mensajeBienvenida = Label(frameP3, text = "Bienvenido al Sistema de Información Hospitalario!", wraplength = 250, font = ("Arial", 20))
+mensajeBienvenida.pack(expand = True, fill = "both")
 
 # Frame P4.
 
-frameP4 = Frame(frameP2, bg = "white", height = 200)
-frameP4.pack(fill = "both", padx = 10, pady = (10, 5))
+frameP4 = Frame(frameP1, bg = "white")
+frameP4.pack(expand = True, fill = "both", padx = 10, pady = (5, 10))
 
 # Frame P5.
 
-frameP5 = Frame(frameP1, bg = "white")
-frameP5.pack(expand = True, fill = "both", padx = 10, pady = (5, 10))
+frameP5 = Frame(frameP2, bg = "white", height = 200)
+frameP5.pack_propagate(False)
+frameP5.pack(fill = "both", padx = 10, pady = (10, 5))
+
+# Hojas de vidas de los desarrolladores en el Frame 5.
+
+hojasDeVida = [
+    "Hojas de Vida de los Desarrolladores",
+    "Nombre: Nombre1\nEdad: Edad1\nVive en: Lugar1\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
+    "Nombre: Nombre2\nEdad: Edad2\nVive en: Lugar2\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
+    "Nombre: Nombre3\nEdad: Edad3\nVive en: Lugar3\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
+    "Nombre: Nombre4\nEdad: Edad4\nVive en: Lugar4\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin"
+]
+indice = 0
+
+descripcionDesarrollador = Label(frameP5, text = "Click!", justify = "left")
+descripcionDesarrollador.pack(expand = True,fill = "both")
+descripcionDesarrollador.bind("<Button-1>", mostrarHV)
 
 # Frame P6.
 
