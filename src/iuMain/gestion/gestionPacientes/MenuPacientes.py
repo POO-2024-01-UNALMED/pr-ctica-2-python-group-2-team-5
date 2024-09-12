0960f55c451b6f3fb2e31dec58472911d946a324
Chocorramosaurus <159493300+PabloJimenez028@users.noreply.github.com>
2024-09-11 23:29:52 -0500

    Ayuda Dios

--- src/iuMain/gestion/gestionPacientes/MenuPacientes.py
+++ src/iuMain/gestion/gestionPacientes/MenuPacientes.py
@@ -1,1 +1,21 @@
+from src.iuMain.gestion.gestionPacientes.GestionPaciente import GestionPaciente
+
+
 class MenuPacientes:
+    @staticmethod
+    def mostrarMenuGestionPacientes(hospital):
+        while True:
+            print("MenuGestion Pacientes")
+            print("1. Registrar paciente")
+            print("2. Registrar enfermedad")
+            print("3. Eliminar paciente")
+            print("4. Ver paciente")
+            print("5. Regresar al menú anterior")
+            print("6. Salir")
+            opcion = int(input("Opcion: "))
+
+            if opcion == 1:
+                GestionPaciente.registrarPaciente(hospital)
+                break
+            elif opcion == 2:
+                GestionPaciente.reg
