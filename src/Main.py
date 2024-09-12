7d351ee4f08bf8f92bf9fd50bf3115ee4d0df64f
Chocorramosaurus <159493300+PabloJimenez028@users.noreply.github.com>
2024-09-11 22:49:50 -0500

    Main

---
+++ src/Main.py
@@ -0,0 +1,25 @@
+from gestorAplicacion.administracionHospital.Hospital import Hospital
+
+def mostrarMenuInicial(hospital)
+    while True:
+        print("MENU INICIAL")
+        print("1. Servicios para Paciente")
+        print("2. Gestionara registros")
+        print("3. Salir")
+
+        opcionSeleccionada = int(input("Seleccione una opcion: "))
+        if opcionSeleccionada == 1:
+            MenuFuncionalidades.
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    hospital = Hospital()
+    mostrarMenuInicial(hospital)
+
+
+
