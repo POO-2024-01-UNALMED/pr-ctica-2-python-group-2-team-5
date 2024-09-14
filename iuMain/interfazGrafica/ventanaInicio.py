24e8a7fccd5631af3a85f10d76207b58118591fc
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-09-14 13:20:33 -0500

    Cambio imagenes FrameP4, cambio de icono ventana

--- iuMain/interfazGrafica/ventanaInicio.py
+++ iuMain/interfazGrafica/ventanaInicio.py
@@ -3,32 +3,41 @@ from PIL import Image, ImageTk
 
 # Primeras pruebas interfaz gráfica python.
 
-# TODO: Añadir los command a los menús.
-
 # Ventana de inicio (Tamaño: 600x600, en una ubicación de x: 400px, y: 40px de la pantalla)
 
 ventanaInicio = Tk()
 ventanaInicio.title("Ventana de Inicio")
 ventanaInicio.geometry("600x600+400+40")
 
+#Cambiar el icono.
+
+ventanaInicio.wm_iconbitmap("imagenes/hospital-building.ico")
+
 # ------------ EVENTOS (PRUEBAS) -------------------
 
-# Eventos del menú.
+# Eventos del menú, muestra la descripción del programa al dar click en descripcion sistema.
 
 def descripcionSistema():
     global mensajeBienvenida
-    mensajeBienvenida.config(text = "Puede pedir citas, generar fórmulas y reservar habitaciones...")
+    mensajeBienvenida.config(text = "Con este programa podrá pedir citas, generar fórmulas médicas, reservar habitaciones, agendar citas de vacunación y realizar los pagos por los servicios recibidos, además de administrar de forma general todos los componentes que hacen parte del sistema hospitalario.", font = ("Arial", 11))
 
-# Método para cargar las imagenes.
+# Método para volver a mostrar el mensaje de bienvenida (Quitar la descripción).
+def quitarDescripcion(event):
+    mensajeBienvenida.config(text = "Bienvenido al Sistema de Información Hospitalario!", font = ("Arial", 15))
+# Métodos para cargar las imagenes.
 
-def cargarImagen(ruta, tamaño = (120, 160)):
+# Para el FrameP6.
+
+def cargarImagen(ruta, tamano = (120, 160)):
     img1 = Image.open(ruta)
-    img1 = img1.resize(tamaño)
+    img1 = img1.resize(tamano)
     return ImageTk.PhotoImage(img1)
 
-def cargarImagenP4(ruta):
+# Para el FrameP4.
+
+def cargarImagenP4(ruta, tamano = (260, 340)):
     img1 = Image.open(ruta)
-    img1 = img1.resize(tamaño)
+    img1 = img1.resize(tamano)
     return ImageTk.PhotoImage(img1)
 
 # Evento para cambiar el texto en las hojas de vida, Frame P5.
@@ -43,23 +52,27 @@ def mostrarHV(event):
     # Para cambiar las imagenes del frameP6.
 
     global indiceImgs, labelImg1, labelImg2, labelImg3, labelImg4, listaImgs
-    # Cambiar las imágenes de cada label independientemente
-    labelImg1.config(image=listaImgs[indiceImgs][0])
-    labelImg2.config(image=listaImgs[indiceImgs][1])
-    labelImg3.config(image=listaImgs[indiceImgs][2])
-    labelImg4.config(image=listaImgs[indiceImgs][3])
+    # Cambiar las imágenes de cada label.
+    labelImg1.config(image = listaImgs[indiceImgs][0])
+    labelImg2.config(image = listaImgs[indiceImgs][1])
+    labelImg3.config(image = listaImgs[indiceImgs][2])
+    labelImg4.config(image = listaImgs[indiceImgs][3])
 
-    # Avanzar al siguiente conjunto de imágenes
+    # Siguiente grupo de imagenes.
+
     indiceImgs = (indiceImgs + 1) % len(listaImgs)
 
-def imagenesP4(event):
-    global imgsFunc, labelImgsFunc, indiceFunc
-    img1 = Image.open(imgsFunc[indiceFunc])
-    #img1 = img1.resize()
-    imgTk = ImageTk.PhotoImage(img1)
-    labelImgsFunc.config(image = imgTk)
+# Método para abrir la ventana al presionar en el botón del FrameP4.
 
-    print("Cambiando imgs")
+def abrirVentPrincipal():
+    pass
+
+# Método para cambiar las imagenes del FrameP4 al pasar el ratón.
+
+def imagenesP4(event):
+    global imgsFunc, btnImgsFunc, indiceFunc
+    btnImgsFunc.config(image = imgsFunc[indiceFunc])
+    indiceFunc = (indiceFunc + 1) % len(imgsFunc)
 
 # --------------------------------------------------
 
@@ -79,7 +92,7 @@ menuInicio.add_command(label = "Descripcion del sistema", command = descripcionS
 menuInicio.add_separator()
 menuInicio.add_command(label = "Salir", command = ventanaInicio.destroy)
 
-# Frames P1 y P2.
+# ------------------- Frames P1 y P2 -----------------------------------------
 
 frameP1 = Frame(ventanaInicio, bg = "black")
 frameP1.pack(side = "left", expand = True, fill = "both", padx = (10, 5), pady = 10)
@@ -87,7 +100,7 @@ frameP1.pack(side = "left", expand = True, fill = "both", padx = (10, 5), pady =
 frameP2 = Frame(ventanaInicio, bg = "black")
 frameP2.pack(side = "right", expand = True, fill = "both", padx = (5, 10), pady = 10)
 
-# Frame P3.
+# ----------------------------- Frame P3 --------------------------------------
 
 frameP3 = Frame(frameP1, bg = "white", height = 200)
 frameP3.pack_propagate(False)
@@ -95,32 +108,33 @@ frameP3.pack(fill = "both", padx = 10, pady = (10, 5))
 
 # Mensaje de bienvenida en el Frame 3.
 
-mensajeBienvenida = Label(frameP3, text = "Bienvenido al Sistema de Información Hospitalario!", wraplength = 250, font = ("Arial", 15))
+mensajeBienvenida = Label(frameP3, text = "Bienvenido al Sistema de Información Hospitalario!", wraplength = 250, font = ("Arial", 15), justify = "left")
 mensajeBienvenida.pack(expand = True, fill = "both")
+mensajeBienvenida.bind("<Button-1>", quitarDescripcion)
 
-# Frame P4.
+# ------------------------------- Frame P4 -------------------------------------
 
 frameP4 = Frame(frameP1, bg = "white", height = 440)
 frameP4.pack_propagate(False)
 frameP4.pack(fill = "both", padx = 10, pady = (5, 10))
 
-# Imagenes para ubicar en el Label del FrameP4.
+# Imagenes para ubicar en el Label del FrameP4
 
-imgsFunc = ["imagenes/fun1.jpg",
-            "imagenes/fun2.jpg",
-            "imagenes/fun3.jpg",
-            "imagenes/fun4.jpg",
-            "imagenes/fun5.jpg"]
+imgsFunc = [cargarImagenP4("imagenes/fun1.jpg"),
+            cargarImagenP4("imagenes/fun2.jpg"),
+            cargarImagenP4("imagenes/fun3.jpg"),
+            cargarImagenP4("imagenes/fun4.jpg"),
+            cargarImagenP4("imagenes/fun5.jpg")]
 
 indiceFunc = 0
 
-# Label para ubicar las imagenes relacionadas al sistema de hospital.
+# Botón para ubicar las imagenes relacionadas al sistema de hospital.
 
-labelImgsFunc = Label(frameP4, bg = "green")
-labelImgsFunc.pack(expand = True, fill = "both", padx = 5, pady = 5)
-labelImgsFunc.bind("<Enter>", imagenesP4)
+btnImgsFunc = Button(frameP4, bg = "green", command = abrirVentPrincipal)
+btnImgsFunc.pack(expand = True, fill = "both", padx = 5, pady = 5)
+btnImgsFunc.bind("<Enter>", imagenesP4)
 
-# Frame P5.
+# ------------------------------ Frame P5 -----------------------------
 
 frameP5 = Frame(frameP2, bg = "white", height = 200)
 frameP5.pack_propagate(False)
@@ -130,18 +144,18 @@ frameP5.pack(fill = "both", padx = 10, pady = (10, 5))
 
 hojasDeVida = [
     "Hojas de Vida de los Desarrolladores",
-    "Nombre: Nombre1\nEdad: Edad1\nVive en: Lugar1\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
-    "Nombre: Nombre2\nEdad: Edad2\nVive en: Lugar2\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
-    "Nombre: Nombre3\nEdad: Edad3\nVive en: Lugar3\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin",
-    "Nombre: Nombre4\nEdad: Edad4\nVive en: Lugar4\nEstudia: Ing. de Sistemas\nEn la Universidad: Nacional de Colombia\nSede: Medellin"
+    "Nombre: Nombre1\nEdad: Edad1\nVive en: Lugar1\nEstudia: Ingeniería de Sistemas\nEn: Universidad Nacional de Colombia\nSede: Medellin\nCorreo: correoinst@unal.edu.co",
+    "Nombre: Nombre2\nEdad: Edad2\nVive en: Lugar2\nEstudia: Ingeniería de Sistemas\nEn: Universidad Nacional de Colombia\nSede: Medellin\nCorreo: correoinst@unal.edu.co",
+    "Nombre: Nombre3\nEdad: Edad3\nVive en: Lugar3\nEstudia: Ingeniería de Sistemas\nEn: Universidad Nacional de Colombia\nSede: Medellin\nCorreo: correoinst@unal.edu.co",
+    "Nombre: Nombre4\nEdad: Edad4\nVive en: Lugar4\nEstudia: Ingeniería de Sistemas\nEn: Universidad Nacional de Colombia\nSede: Medellin\nCorreo: correoinst@unal.edu.co"
 ]
 indice = 0
 
-hvDesarrolladores = Label(frameP5, text = "Click!", justify = "left")
+hvDesarrolladores = Label(frameP5, text = "Click!", justify = "left", font = ("Arial", 11), wraplength = 250)
 hvDesarrolladores.pack(expand = True,fill = "both")
 hvDesarrolladores.bind("<Button-1>", mostrarHV)
 
-# Frame P6.
+# ---------------------------- Frame P6 --------------------------------------
 
 frameP6 = Frame(frameP2, bg = "white", height = 440)
 frameP6.grid_propagate(False)
@@ -162,20 +176,20 @@ listaImgs = [
 
 indiceImgs = 0
 
-# Label para ubicar la imagem.
+# Label para ubicar las imagemes.
 
 labelImg1 = Label(frameP6, image = img1Tk, background = "blue")
-labelImg1.grid(row = 0, column = 0, padx = (5, 2), pady = (5, 2))
+labelImg1.grid(row = 0, column = 0, padx = (6, 2), pady = (8, 2))
 
 labelImg2 = Label(frameP6, image = img1Tk, background = "blue")
-labelImg2.grid(row = 0, column = 1, padx = (2, 5), pady = (5, 2))
+labelImg2.grid(row = 0, column = 1, padx = (3, 5), pady = (8, 2))
 
 labelImg3 = Label(frameP6, image = img1Tk, background = "blue")
-labelImg3.grid(row = 1, column = 0, padx = (5, 2), pady = (2, 5))
+labelImg3.grid(row = 1, column = 0, padx = (6, 2), pady = (5, 5))
 
 labelImg4 = Label(frameP6, image = img1Tk, background = "blue")
-labelImg4.grid(row = 1, column = 1, padx = (2, 5), pady = (2, 5))
+labelImg4.grid(row = 1, column = 1, padx = (3, 5), pady = (5, 5))
 
-# Método mainloop.
+# -------------------------- Método mainloop ----------------------------------
 
 ventanaInicio.mainloop()
