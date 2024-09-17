d2d501aa6cfb324b406fe32b731e3ea810cb0156
Chocorramosaurus <159493300+PabloJimenez028@users.noreply.github.com>
2024-09-17 08:10:14 -0500

    Se me olvido que desarregle el Main

--- src/Main.py
+++ src/Main.py
@@ -1,25 +1,6 @@
-import pickle
-
 from gestorAplicacion.administracionHospital.Hospital import Hospital
 from iuMain.interfazGrafica.ventanaInicio import abrirVentanaInicio
 
 if __name__ == '__main__':
-
-    serializarDoctores = open("../../baseDatos/temp/registro_doctores.pickle", "wb")
-    pickle.dump(None, serializarDoctores)
-    serializarDoctores.close()
-    serializarPacientes = open("../../baseDatos/temp/registro_pacientes.pickle", "wb")
-    pickle.dump(None, serializarPacientes)
-    serializarPacientes.close()
-    serializarMedicamento = open("../../baseDatos/temp/registro_medicamentos.pickle", "wb")
-    pickle.dump(None, serializarMedicamento)
-    serializarMedicamento.close()
-    serializarVacunas = open("../../baseDatos/temp/registro_vacunas.pickle", "wb")
-    pickle.dump(None, serializarVacunas)
-    serializarVacunas.close()
-    serializarEnfermedades = open("../../baseDatos/temp/registro_enfermedades.pickle", "wb")
-    pickle.dump(None, serializarEnfermedades)
-    serializarEnfermedades.close()
-    serializarHabitaciones = open("../../baseDatos/temp/registro_habitaciones.pickle", "wb")
-    pickle.dump(None, serializarHabitaciones)
-    serializarHabitaciones.close()
+    hospitalAndino = Hospital()
+    abrirVentanaInicio(hospitalAndino)
